340f6bc7131dfdf7ef570ff67858a7a9808b7c56
Razbuer <Rusyaevmax@yandex.ru>
2022-08-09 13:54:40 +0300

    Приложение должно быть консольным - считывать выражение с клавиатуры

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(calc("1 + 2"));          // Output: 3
-        System.out.println(calc("VI / III"));       // Output: II
-        //System.out.println(calc("I - II"));         // Output: throws Exception //т.к. в римской системе нет отрицательных чисел
-        //System.out.println(calc("I + 1"));          // Output: throws Exception //т.к. используются одновременно разные системы счисления
-        //System.out.println(calc("1"));              // Output: throws Exception //т.к. строка не является математической операцией
-        //System.out.println(calc("1 + 2 + 3"));      // Output: throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)
+        Scanner scanner = new Scanner(System.in);
+        String expression;
+
+        System.out.println("Это калькулятор. По окончанию введите \"exit\"");
+
+        while(!(expression = scanner.nextLine()).equals("exit")) {
+            System.out.println(calc(expression));
+        }
 
     }
 
